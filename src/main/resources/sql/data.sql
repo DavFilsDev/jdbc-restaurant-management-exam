f73c0f39fbb89f3e523813dbf4544d062288391b
DavFilsDev <miharisoadavidfils@gmail.com>
2026-01-06 15:30:20 +0300

    add: inside data.sql all script used in the insertion of the data

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,15 @@
+-- Insert data into Dish table
+insert into dish (id, name, dish_type)
+values  (1, 'Salade fraîche', 'START'),
+        (2, 'Poulet grillé', 'MAIN'),
+        (3, 'Riz aux légumes', 'MAIN'),
+        (4, 'Gâteau au chocolat', 'DESSERT'),
+        (5, 'Salade de fruits', 'DESSERT');
+
+-- Insert data into Ingredient table
+insert into ingredient (id, name, price, category, id_dish)
+values  (1, 'Latue', 800.00, 'VEGETABLE', 1),
+        (2, 'Tomate', 600.00, 'VEGETABLE', 1),
+        (3, 'Poulet', 4500.00, 'ANIMAL', 2),
+        (4, 'Chocolat', 3000.00, 'OTHER', 4),
+        (5, 'Beurre', 2500.00, 'DAIRY', 4);
