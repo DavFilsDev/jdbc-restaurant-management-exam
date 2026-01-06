0f879e9633620278a392e1f7e64c21f1b61477fb
DavFilsDev <miharisoadavidfils@gmail.com>
2026-01-06 15:46:13 +0300

    update: fix error in the creation table script in schema.sql file and ajust the code format

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,6 +1,5 @@
 create type dish_type_enum as enum ('START', 'MAIN', 'DESSERT');
 
-
 create table dish (
                       id serial constraint dish_pk primary key,
                       name varchar(255),
@@ -10,9 +9,8 @@ create table dish (
 
 create type ingredient_category as enum ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
 
-
 create table ingredient (
-                            id serial ingredient_pk primary key,
+                            id serial constraint ingredient_pk primary key,
                             name varchar(255),
                             price numeric(10, 2) CHECK (price >= 0),
                             category ingredient_category,
